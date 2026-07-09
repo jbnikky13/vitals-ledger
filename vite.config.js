bff003db2dac09a970ba352f2aaf6f7753f51227
Nichole Godam <79281031+jbnikky13@users.noreply.github.com>
2026-07-09 20:28:56 +0000

    Add base path and deploy script for Github Pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
+  base: '/vitals-ledger/',
   plugins: [
     VitePWA({
       registerType: 'autoUpdate',
